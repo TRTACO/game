43951b5436a0d372bd6414b1cc20af88b3538bc8
Christian Tschoepe <tschoepe.christian@gmail.com>
2013-04-27 02:45:53 -0500

    Added a page to the site and made it much more awesome
    Everything's much more organized now and is much more appealing!

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,20 @@
+$(document).ready(function(){
+        //$('title').fade('slow', 1);
+        //$('button').mouseenter(function(){
+        //        $(this).css({
+        //            'color':'red'
+        //        });
+        //});
+        //$('button').mouseleave(function(){
+        //        $(this).css({
+        //            'color':'black'
+        //        });
+        //});
+		
+		//$('p').append('<div>text</div>');
+		$('h2').css({ 'color':'red'});
+		$('h1').after("<div>Hey, what's up!?!</div>");
+
+});
+
 console.log('This would be the main JS file.');
